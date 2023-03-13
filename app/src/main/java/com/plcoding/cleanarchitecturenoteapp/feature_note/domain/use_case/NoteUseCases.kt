a9a347f7969277ecd1927ebeb8b87c65c85a043d
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-03-13 21:18:56 +0000

    initial commit and added upto to the repository

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -2,5 +2,7 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 
 data class NoteUseCases(
     val getNotesUseCase: GetNotesUseCase,
-    val deleteNoteUseCase: DeleteNoteUseCase
+    val deleteNoteUseCase: DeleteNoteUseCase,
+    val addNoteUseCase: AddNoteUseCase,
+    val getNote: GetNote
 )
